8a3c9f9098f3d8c8442d2f3530e28c49bbfbfe05
Moisés Xavier <secondmox91@gmail.com>
2024-07-09 20:29:10 -0300

    feat: Adiciona arquivos de projetos do curso

---
+++ Lógica de Programação/logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert("Olá mundo");
+let n1 = 23
+let valueP = prompt('qual o numero secreto?')
+
+if (n1 == valueP ) {
+    alert("Parabens voce acertou o numero secreto! \n Numero secreto: " + n1)
+} else {
+    alert("numero errado")
+}
